96e62b07997c9089ff730a4da12d62ce867eb881
marcushcarter <marcoose408@gmail.com>
2026-02-17 13:48:50 -0500

    source way

--- src/main.js
+++ src/main.js
@@ -2,13 +2,59 @@ const canvas = document.getElementById('glCanvas');
 const gl = canvas.getContext('webgl2');
 if (!gl) { alert("WebGL2 not supported"); }
 
-function loadFromFile(url) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, false);
-    xhr.send();
-    if (xhr.status !== 200) throw new Error('Failed to load ' + url);
-    return xhr.responseText;
+const htVertexSrc = `#version 300 es
+precision highp float;
+
+out vec3 color;
+
+void main() {
+    vec2 positions[3];
+    positions[0] = vec2( 0.0,  0.5);
+    positions[1] = vec2(-0.5, -0.5);
+    positions[2] = vec2( 0.5, -0.5);
+
+    gl_Position = vec4(positions[gl_VertexID], 0.0, 1.0);
+
+    color = vec3(float(gl_VertexID == 0), float(gl_VertexID == 1), float(gl_VertexID == 2));
 }
+`;
+
+const htFragmentSrc = `#version 300 es
+precision highp float;
+
+in vec3 color;
+out vec4 fragColor;
+
+void main() {
+    fragColor = vec4(color, 1.0); // interpolated color
+}
+`;
+
+const blitVertexSrc = `#version 300 es
+precision highp float;
+out vec2 vUV;
+void main() {
+    vec2 positions[3] = vec2[3](
+        vec2(-1.0, -1.0),
+        vec2(3.0, -1.0),
+        vec2(-1.0, 3.0)
+    );
+    gl_Position = vec4(positions[gl_VertexID], 0.0, 1.0);
+    vUV = gl_Position.xy * 0.5 + 0.5;
+}
+`;
+
+const blitFragmentSrc = `#version 300 es
+precision highp float;
+
+in vec2 vUV;
+uniform sampler2D uTex;
+out vec4 fragColor;
+
+void main() {
+    fragColor = texture(uTex, vUV);
+}
+`;
 
 class Shader {
     constructor() {}
@@ -61,10 +107,10 @@ class Shader {
 }
 
 const htShader = new Shader();
-htShader.create(gl, loadFromFile('../assets/shaders/ht.vert'), loadFromFile('../assets/shaders/ht.frag'));
+htShader.create(gl, htVertexSrc, htFragmentSrc);
 
 const blitShader = new Shader();
-blitShader.create(gl, loadFromFile('../assets/shaders/blit.vert'), loadFromFile('../assets/shaders/blit.frag'));
+blitShader.create(gl, blitVertexSrc, blitFragmentSrc);
 
 const vao = gl.createVertexArray();
 gl.bindVertexArray(vao);
